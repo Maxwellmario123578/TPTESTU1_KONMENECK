f15ce52bb61042f9a8c0d65fa2bf1dc7d5994795
Maxwellmario123578 <konmeneckmasewellmathurin@gmail.com>
2025-11-11 20:35:21 +0100

    feat: Implementation de la methode trier

--- src/main/java/com/saintjean/operation/FactorielInvalidException.java
+++ src/main/java/com/saintjean/operation/FactorielInvalidException.java
@@ -1,7 +1,7 @@
-package operation;
+package com.saintjean.operation;
 
 public class FactorielInvalidException extends Exception {
     public FactorielInvalidException(String message) {
-        super(message);
+        super(message); 
     }
 }
