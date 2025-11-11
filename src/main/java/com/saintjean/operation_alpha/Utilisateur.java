5092c44c839a580817ea23e7d6bd57c24564a366
Maxwellmario123578 <konmeneckmasewellmathurin@gmail.com>
2025-11-11 21:31:14 +0100

    feat: Creation de la classe utilisateur

---
+++ src/main/java/com/saintjean/operation_alpha/Utilisateur.java
@@ -0,0 +1,13 @@
+package com.saintjean.operation_alpha;
+
+public class Utilisateur {
+    private int id;
+    private String nom;
+    private int age;
+    private String email;
+    private String telephone;
+    private String ville;
+    private double soldePersonnel;
+
+}
+
