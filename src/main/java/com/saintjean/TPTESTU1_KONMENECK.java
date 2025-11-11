14e73790446bb1d61cf1ec8ea08fa521b1fa6b40
Maxwellmario123578 <konmeneckmasewellmathurin@gmail.com>
2025-11-11 19:30:27 +0100

    initialisation du projet

---
+++ src/main/java/com/saintjean/TPTESTU1_KONMENECK.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.saintjean;
+
+/**
+ *
+ * @author USER
+ */
+public class TPTESTU1_KONMENECK {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
